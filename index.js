b4d5499380b6645051f45573ea25fdf6ee1e77f4
Romjan Hossain <romjanhossainn@gmail.com>
2023-10-25 22:46:25 +0600

    car-project-setup

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res) =>{
+    res.send('Doctor Running')
+})
+
+const uri =
+  `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.4ievbno.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+
+
+    
+
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.listen(port,() => {
+    console.log('the server is running.')
+})
