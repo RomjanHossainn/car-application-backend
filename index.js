fc9690dcd807683a71a5d14199e02bc54b88b779
Romjan Hossain <romjanhossainn@gmail.com>
2023-10-26 02:15:40 +0600

    service order single service

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion ,ObjectId} = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,8 +29,40 @@ async function run() {
     
     await client.connect();
 
+    const carDataBase = client.db('carDataBase').collection('services');
+    const orderDataBase = client.db("carDataBase").collection('orders');
 
-    
+
+    app.get('/services',async(req,res) => {
+        const resilt = await carDataBase.find().toArray();
+        res.send(resilt)
+    })
+
+    app.get('/services/:id',async(req,res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result = await carDataBase.findOne(query);
+        res.send(result);
+    })
+
+
+
+    app.get("/checkout/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { title: 1, img: 1,price : 1 },
+      };
+      const result = await carDataBase.findOne(query ,options);
+      res.send(result);
+    });
+
+
+    app.post('/postorder',async(req,res) =>{
+      const order = req.body;
+      const result = await orderDataBase.insertOne(order);
+      res.send(result);
+    })
 
    
     await client.db("admin").command({ ping: 1 });
